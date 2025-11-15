786ba1253fc533905285a95db7a26d087947200c
Voxell Paladynee <paladyneshadow@gmail.com>
2025-11-15 08:28:10 +0100

    hello-world: Enhancements!

--- content/_01-hello-world/src/main.rs
+++ content/_01-hello-world/src/main.rs
@@ -1,31 +1,65 @@
 //! Rust program to print hello world.
 //!
-//! This program, on invocation, prints the bytes "Hello, world!" to the standard output
-//! buffer and flushes it, and then immediately terminates. The program may panic if
-//! writing to the standard output fails
+//! keywords: hello world, program, main, function, print
 //!
-//! Usage: hello_world.exe
+//! This program, on invocation, prints the bytes "Hello, world!" to the standard output
+//! buffer (also known as the terminal, the console, the CLI, the tty or the teletypewriter)
+//! and flushes it, and then exits. The program may panic if writing to the standard
+//! output fails.
+//! more on panics later.
+//!
+//! Usage (in the terminal):
+//!
+//! # using Cargo, Rust's package manager
+//!     cargo run
+//! # precompiled program
+//!     # windows
+//!     hello_world.exe
+//!     # linux
+//!     ./hello_world
 //!
 //! Voxell Paladynee 2025
 
-// the entry point of our program. the standard library will call this function upon program
-// invocation. it can return any value that implements `Terminate`. `()` is one of them,
-// so we don't return anything.
+// +-------------------------------------------------+
+// | Rust Editor.exe                           _ o x |
+// +----|--------------------------------------------+
+// | 32 |                                            |
+// | 33 | fn main() {                                |
+// | 31 | --------- `main` is a special function!    |
+// +----|--------------------------------------------+
+//
+// the entry point of our program, named `main`. the standard library will automatically call this function
+// upon program invocation, so it is a special function. it can return any value that implements `Terminate`.
+// this function returns nothing, and nothing is one such type.
+// more on `impl`ementations, `functions` and the `nothing` type later.
 fn main() {
-    // we're inside the main function, and we encounter our first scope with a non-'static lifetime.
-    // anything declared in this function does not live after main exits.
+    // we're inside the `main` function, and we encounter our first scope with a non-'static lifetime.
+    // anything declared in this function, like variables, do not "live" after main exits.
+    // more on `lifetimes`, `'static` and `scopes` later.
 
-    // the standard library and the `Drop` implementations of some user-written structs
-    // may do some cleanup after the main function ends, after then which the program
-    // program terminates.
+    // the standard library and user-written types that have `Drop` implementations
+    // may do some cleanup after the main function exits, after then which the program
+    // terminates automatically.
+    // more on `Drop` later.
 
-    // here we define a string literal with an inferred size. the string literal actually
-    // has 'static lifetime, because it is actually a pointer to the to-be-compiled binary,
-    // which exists regardless of whether the program is running or not. therefore,
+    //      the &str type
+    //     +--------+--------+
+    //     | 0x0123 |     13 |
+    //     +--------+--------+
+    //          |
+    //          v
+    //     +--------+--------+--------+--------+--------+--------+
+    //     |    'H' |    'e' |    'l' |    'l' |    'o' |    ' ' | ...
+    //     +--------+--------+--------+--------+--------+--------+
+    //      hello.exe contents
+    
+    // here we define a `string literal` with an inferred size. the string literal actually
+    // has 'static lifetime, because it is actually a pointer to the to-be-compiled binary program,
+    // which exists regardless of whether we're inside `main` or not. therefore,
     // Rust allows us to make values with 'static lifetimes.
 
     // the `&str` type holds a compile-time guaranteed UTF-8 valid sequence of bytes,
-    // therefore it is always safe for us to push this value to the standard output.
+    // therefore it is always safe for us to write this value to the standard output.
     let string: &'static str = "Hello, world!";
 
     // here we invoke the `println!` macro, which is included with the Rust standard library
